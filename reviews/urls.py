742ad8770345b4c79e2f8c5a4bb2d9efe929876a
taehopark <qkrxogh7@gmail.com>
2022-11-01 16:05:25 +0900

    comment_create/delete...ing 만드는 중!

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,7 +6,7 @@ app_name = 'reviews'
 urlpatterns = [
      path('', views.index, name='index'),
      path('create/',views.create, name='create'),
-     path('<int:review_pk>/comment/', views.comment_create,name='comment_create'),
-#     path('<int:review_pk>/comment/<int:comment_pk>/delete/', views.comment_delete,name='comment_delete'),
+     path('<int:detail_pk>/comment/', views.comment_create,name='comment_create'),
+    path('<int:detail_pk>/comment/<int:comment_pk>/delete/', views.comment_delete,name='comment_delete'),
 
 ]
