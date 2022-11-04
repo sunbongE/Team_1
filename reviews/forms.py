f5912676d6ae21be850b26dc60cd2307d759fb9c
mechauk418 <mechauk418@gmail.com>
2022-11-04 10:03:13 +0900

    1104front

--- reviews/forms.py
+++ reviews/forms.py
@@ -16,6 +16,17 @@ class ReviewForm(forms.ModelForm):
             'breaktime',
             'content',
         )
+        labels={
+            'store_name':'상호명',
+            'store_address':'주소',
+            'image':'사진',
+            'phoneNumber':'가게 번호',
+            'parking':'주차',
+            'time':'영업 시간',
+            'price':'가격대',
+            'breaktime':'휴식 시간',
+            'content':'가게 소개',
+        }
 
 class CommentForm(forms.ModelForm):
 
