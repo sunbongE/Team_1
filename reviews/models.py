757c78cb116671b696bc1bec7bafa578b0952a62
taehopark <qkrxogh7@gmail.com>
2022-11-07 14:55:00 +0900

    model

--- reviews/models.py
+++ reviews/models.py
@@ -12,7 +12,7 @@ class Review(models.Model):
     image = models.ImageField(upload_to='images/', blank=True)    # 메뉴 사진
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)              # 즐겨찾기
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews') # 즐겨찾기
-    phoneNumberRegex = RegexValidator(regex = r'([0-9]{2,4})-?([0-9]{3,4})-?([0-9]{4})')   # 폰번
+    phoneNumberRegex = RegexValidator(regex = str(r'([0-9]{2,4})-?([0-9]{3,4})-?([0-9]{4})'))   # 폰번
     phoneNumber = models.CharField(validators = [phoneNumberRegex], max_length = 16, unique = True)
     parking = models.BooleanField(default=False)                   # 주차가능 유무
     price = models.CharField(max_length=15)                         # 가격대
